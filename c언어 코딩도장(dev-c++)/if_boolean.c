857ebaf9bc78333c554d750f459479dd3f2c58b1
KIDOKY <118824913+KIDOKY@users.noreply.github.com>
2024-08-07 23:22:11 +0900

    Add files via upload

---
+++ c언어 코딩도장(dev-c++)/if_boolean.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	if(true)
+		printf("Âü\n");
+	else
+		printf("°ÅÁş\n");
+	
+	if(false)
+		printf("Âü\n");
+	else
+		printf("°ÅÁş\n");
+		
+		return 0;
+}
